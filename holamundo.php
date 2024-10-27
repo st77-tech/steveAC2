e12a0772946e0019fb303db2c15b351f2c62db15
steveAC2 <steve_m77@hotmail.fr>
2024-10-27 22:54:01 +0000

    Tercer commit

--- holamundo.php
+++ holamundo.php
@@ -1,12 +1,13 @@
 <html>
-<head></head>
+<head>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
 <body>
-  <?php
-  echo "Hola Mundo!";
-  ?>
+    <div class="message">
+        <?php echo "Hola Mundo!"; ?>
+    </div>
 </body>
 </html>
-
 
 
 
